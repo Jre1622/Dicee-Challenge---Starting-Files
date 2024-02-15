fb172fda3ddce36e3be1bdabfd7197cab75acd16
John <jre1622@gmail.com>
2024-02-15 13:15:17 +0700

    winner JS

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ let randomNum = Math.floor(Math.random() * 6) + 1;
 let randomNumTwo = Math.floor(Math.random() * 6) + 1;
 
 //Select the image
-let diceImage = document.querySelector(".dice .img1");
+let diceImage = document.querySelector(".dice .img1"); //Select the image with class img1 child of class dice
 let diceImageTwo = document.querySelector(".dice .img2");
 
 //Change the image
-diceImage.src = `images/dice${randomNum}.png`;
-diceImageTwo.src = `images/dice${randomNumTwo}.png`;
+diceImage.src = "./images/dice" + randomNum + ".png";
+diceImageTwo.src = "./images/dice" + randomNumTwo + ".png";
+
+//Select the h1
+let h1 = document.querySelector("h1"); //Grab the large "Refresh Me"
+
+//Change the h1
+if (randomNum > randomNumTwo) {
+	h1.innerHTML = "🚩Player 1 Wins!";
+} else if (randomNum < randomNumTwo) {
+	h1.innerHTML = "Player 2 Wins!🚩";
+} else {
+	h1.innerHTML = "Draw!";
+}
